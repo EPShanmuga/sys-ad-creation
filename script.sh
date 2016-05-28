d85dc961ab98193b321ca7a961fb22bf42a88287
EPShanmuga <shanmugaep8@gmail.com>
2016-05-28 22:42:08 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,7 +9,13 @@ while [ $COUNTER -lt 100 ]; do
 cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1 > ./mydir/file$COUNTER
 #changing date of creation
 touch -d "2 days ago"  ./mydir/file$COUNTER
+#extend the size to 10kb
+truncate -s 10k ./mydir/file$COUNTER
 let COUNTER=COUNTER+1
 done
 #changing into read-only access
 chmod 444 ./mydir/*
+
+
+
+
